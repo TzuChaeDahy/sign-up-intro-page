7a80386b03f132520bcbc2c3152aec5f134919b4
TzuChaeDahy <viniciuslovestwice@gmail.com>
2024-09-27 13:02:49 -0300

    feat: implement input tooltiping and refactor reducer logic

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -7,78 +7,92 @@ export default function Form() {
     first: {
       value: "",
       valid: false,
+      message: "",
     },
     last: {
       value: "",
       valid: false,
+      message: "",
     },
     email: {
       value: "",
       valid: false,
+      message: "",
     },
     password: {
       value: "",
       valid: false,
+      message: "",
     },
   };
 
   const [formState, dispatch] = useReducer(formReducer, initialState);
 
-  function firstNameChange(
+  function inputChange(
     event: ChangeEvent<HTMLInputElement>,
     inputType: InputType
   ) {
     dispatch({ type: inputType, payload: event.target.value });
   }
 
-  console.log(formState);
-
   return (
     <form className={styles.form}>
-      <input
-        type="text"
-        placeholder="First Name"
-        className={
-          !formState.first.valid
-            ? styles.invalid + " " + styles.input
-            : styles.input
-        }
-        onChange={(event) => firstNameChange(event, InputType.FIRST)}
-        value={formState.first.value}
-      />
-      <input
-        type="text"
-        placeholder="Last Name"
-        className={
-          !formState.last.valid
-            ? styles.invalid + " " + styles.input
-            : styles.input
-        }
-        onChange={(event) => firstNameChange(event, InputType.LAST)}
-        value={formState.last.value}
-      />
-      <input
-        type="email"
-        placeholder="Email Address"
-        className={
-          !formState.email.valid
-            ? styles.invalid + " " + styles.input
-            : styles.input
-        }
-        onChange={(event) => firstNameChange(event, InputType.EMAIL)}
-        value={formState.email.value}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        className={
-          !formState.password.valid
-            ? styles.invalid + " " + styles.input
-            : styles.input
-        }
-        onChange={(event) => firstNameChange(event, InputType.PASSWORD)}
-        value={formState.password.value}
-      />
+      <div className={styles.wrapper}>
+        <input
+          type="text"
+          placeholder="First Name"
+          className={
+            !formState.first.valid && formState.first.message !== ""
+              ? styles.invalid + " " + styles.input
+              : styles.input
+          }
+          onChange={(event) => inputChange(event, InputType.FIRST)}
+          value={formState.first.value}
+        />
+        <span className={formState.first.valid ? styles.invisible : styles.error}>{formState.first.message}</span>
+      </div>
+      <div className={styles.wrapper}>
+        <input
+          type="text"
+          placeholder="Last Name"
+          className={
+            !formState.last.valid && formState.last.message !== ""
+              ? styles.invalid + " " + styles.input
+              : styles.input
+          }
+          onChange={(event) => inputChange(event, InputType.LAST)}
+          value={formState.last.value}
+        />
+        <span className={formState.last.valid ? styles.invisible : styles.error}>{formState.last.message}</span>
+      </div>
+      <div className={styles.wrapper}>
+        <input
+          type="email"
+          placeholder="Email Address"
+          className={
+            !formState.email.valid && formState.email.message !== ""
+              ? styles.invalid + " " + styles.input
+              : styles.input
+          }
+          onChange={(event) => inputChange(event, InputType.EMAIL)}
+          value={formState.email.value}
+        />
+        <span className={formState.email.valid ? styles.invisible : styles.error}>{formState.email.message}</span>
+      </div>
+      <div className={styles.wrapper}>
+        <input
+          type="password"
+          placeholder="Password"
+          className={
+            !formState.password.valid && formState.password.message !== ""
+              ? styles.invalid + " " + styles.input
+              : styles.input
+          }
+          onChange={(event) => inputChange(event, InputType.PASSWORD)}
+          value={formState.password.value}
+        />
+        <span className={formState.password.valid ? styles.invisible : styles.error}>{formState.password.message}</span>
+      </div>
       <button className={styles.button}>CLAIM YOUR FREE TRIAL</button>
       <p className={styles.terms}>
         By clicking the button, you are agreeing to our{" "}
--- src/hooks/formHooks.ts
+++ src/hooks/formHooks.ts
@@ -21,57 +21,77 @@ export interface FormState {
   first: {
     value: string;
     valid: boolean;
+    message: string;
   };
   last: {
     value: string;
     valid: boolean;
+    message: string;
   };
   email: {
     value: string;
     valid: boolean;
+    message: string;
   };
   password: {
     value: string;
     valid: boolean;
+    message: string;
   };
 }
 
 export function formReducer(state: FormState, action: FormAction) {
   switch (action.type) {
-    case InputType.FIRST:
+    case InputType.FIRST: {
+      const fitRegex = doesItFitRegex(nameRegex, action.payload);
+
       return {
         ...state,
         first: {
           value: action.payload,
-          valid: doesItFitRegex(nameRegex, action.payload),
+          valid: fitRegex,
+          message: fitRegex ? "" : "First name cannot be empty and must have length greater than 2 characters"
         },
       };
+    }
 
-    case InputType.LAST:
+    case InputType.LAST: {
+      const fitRegex = doesItFitRegex(nameRegex, action.payload);
+
       return {
         ...state,
         last: {
           value: action.payload,
-          valid: doesItFitRegex(nameRegex, action.payload),
+          valid: fitRegex,
+          message: fitRegex ? "" : "Last name cannot be empty and must have length greater than 2 characters",
         },
       };
+    }
 
-    case InputType.EMAIL:
+    case InputType.EMAIL: {
+      const fitRegex = doesItFitRegex(emailRegex, action.payload);
+
       return {
         ...state,
         email: {
           value: action.payload,
-          valid: doesItFitRegex(emailRegex, action.payload),
+          valid: fitRegex,
+          message: fitRegex ? "" : "Email must follow a valid pattern",
         },
       };
+    }
 
-    case InputType.PASSWORD:
+    case InputType.PASSWORD: {
+      const fitRegex = doesItFitRegex(passwordRegex, action.payload);
+
       return {
         ...state,
         password: {
           value: action.payload,
-          valid: doesItFitRegex(passwordRegex, action.payload),
+          valid: fitRegex,
+          message: fitRegex ? "" : "Password must have at least 7 normal characters and 1 special",
         },
       };
+    }
   }
 }
